2a88e9d35c0bfed397b914dcd2a914c9e3741d76
teresabowe <teresa.bowe@gmail.com>
2023-04-02 11:42:52 +0000

    feat: accept default image on product model and complete product add functionality

--- products/models.py
+++ products/models.py
@@ -71,7 +71,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(upload_to='media', default="/noimage.png")
     crafter = models.ForeignKey('Crafter', null=True, blank=True, on_delete=models.SET_NULL)
     source = models.ForeignKey('Source', null=True, blank=True, on_delete=models.SET_NULL)
 
