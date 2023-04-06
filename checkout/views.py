f2b75049431f12998ea4f4b9a0f09cb8a1da9980
teresabowe <teresa.bowe@gmail.com>
2023-04-06 14:35:32 +0000

    chore: add import to views for EmailMessages

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
+from django.core.mail import EmailMessage
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
